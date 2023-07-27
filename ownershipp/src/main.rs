7948ae6acefddff25df404bdcfeb918d789337f6
angel-junior <iangeeel01@gmail.com>
2023-07-26 23:36:45 -0400

    ownership

---
+++ ownershipp/src/main.rs
@@ -0,0 +1,21 @@
+fn main(){
+
+
+    //stack
+    let number1 = 51;
+    let number2 = 42;
+
+
+    //heap
+    let mut hello = String::new();
+
+    hello.push_str("hola no se si te acuerdas de mi");
+
+
+    let dueño = String::from("simon");
+    let s2 = dueño.clone();
+
+    println!("{}", dueño);
+    print!("{}", s2);
+
+}
