aac7e35fe42515d7ac634c200d733fb77a3cef60
Angel Abreu <67086889+1Angel@users.noreply.github.com>
2023-07-31 23:46:30 -0400

    Delete examplewithstructs/target directory

--- examplewithstructs/target/debug/deps/examplewithstructs-0a160b3968a1c864.d
+++
@@ -1,5 +0,0 @@
-C:\Users\lenovo\Desktop\LearningRust\examplewithstructs\target\debug\deps\examplewithstructs-0a160b3968a1c864.rmeta: src\main.rs
-
-C:\Users\lenovo\Desktop\LearningRust\examplewithstructs\target\debug\deps\examplewithstructs-0a160b3968a1c864.d: src\main.rs
-
-src\main.rs:
--- examplewithstructs/target/debug/deps/examplewithstructs-8550f831347ab852.d
+++
@@ -1,5 +0,0 @@
-C:\Users\lenovo\Desktop\LearningRust\examplewithstructs\target\debug\deps\examplewithstructs-8550f831347ab852.rmeta: src\main.rs
-
-C:\Users\lenovo\Desktop\LearningRust\examplewithstructs\target\debug\deps\examplewithstructs-8550f831347ab852.d: src\main.rs
-
-src\main.rs:
